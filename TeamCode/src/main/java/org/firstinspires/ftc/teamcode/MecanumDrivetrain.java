273bebc3925da479a874cf2aae3997b549f0355f
Sushirahulk <113491574+Sushirahulk@users.noreply.github.com>
2023-09-22 14:53:35 -0700

    Made a drivetrain code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrivetrain.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp
+public class MecanumDrivetrain extends LinearOpMode{
+    @Override
+    public void runOpMode()  throws InterruptedException{
+
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()){
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
+
+            double demominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y+x+rx)/ demominator;
+            double backLeftPower = (y-x+rx)/demominator;
+            double frontRightPower = (y-x-rx)/demominator;
+            double backRightPower = (y+x-rx)/demominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontLeftMotor.setPower(frontLeftPower);
+            backRightMotor.setPower(backRightPower);
+        }
+
+    }
+
+
+
+}
