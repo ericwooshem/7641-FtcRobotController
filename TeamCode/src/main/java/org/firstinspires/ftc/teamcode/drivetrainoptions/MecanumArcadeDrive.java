05f282dd167c5ed9f586dfb398af2011449562dd
Sushirahulk <113491574+Sushirahulk@users.noreply.github.com>
2023-09-22 20:29:16 -0700

    Added terminal comands in readme

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrainoptions/MecanumArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrainoptions/MecanumArcadeDrive.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.drivetrainoptions;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @TeleOp
-public class MecanumDrivetrain extends LinearOpMode{
+public class MecanumArcadeDrive extends LinearOpMode{
     @Override
     public void runOpMode()  throws InterruptedException{
 
@@ -26,7 +26,7 @@ public class MecanumDrivetrain extends LinearOpMode{
         while (opModeIsActive()){
 
             double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x * 1.1;
+            double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
             double demominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
