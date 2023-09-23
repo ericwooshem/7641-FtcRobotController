380291b5d34e720132e6b3d491c59f457a26adb1
blueseaturtle08 <113743479+blueseaturtle08@users.noreply.github.com>
2023-09-22 21:48:38 -0700

    Update AprilTagVision.java
    help

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AprilTagVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AprilTagVision.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -48,7 +49,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "AprilTagVision", group = "Concept")
+@Autonomous(name = "AprilTagVision", group = "Concept")
 public class AprilTagVision extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
@@ -68,11 +69,9 @@ public class AprilTagVision extends LinearOpMode {
 
         initAprilTag();
 
-        // Wait for the DS start button to be touched.
-        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
-        telemetry.addData(">", "Touch Play to start OpMode");
-        telemetry.update();
+
         waitForStart();
+
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
