d90175967b37c29bdb8fb696e6db00cc57f893d0
Sushirahulk <113491574+Sushirahulk@users.noreply.github.com>
2023-10-13 21:36:01 -0700

    did macros

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueRightAutonOption1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueRightAutonOption1.java
@@ -1,4 +1,48 @@
 package org.firstinspires.ftc.teamcode.auton;
 
-public class BlueRightAutonOption1 {
+import android.transition.Slide;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.Slides;
+
+@TeleOp(name = "BlueRightAutonOption1")
+public class BlueRightAutonOption1 extends LinearOpMode {
+
+    Slides slidelift= new Slides();
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()){
+            double y = -(gamepad1.left_stick_y);
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+            double g2ry = gamepad2.right_stick_y;
+
+
+            if (gamepad1.a){
+                slidelift.slide('a', y,x,rx,g2ry);
+            }
+            else if (gamepad1.x){
+                slidelift.slide('x', y,x,rx,g2ry);
+            }
+            else if (gamepad1.y){
+                slidelift.slide('y', y,x,rx,g2ry);
+            }
+
+
+
+        }
+    }
+
+
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Slides.java
@@ -1,48 +1,63 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp
-public class Slides extends LinearOpMode{
-    @Override
-    public void runOpMode()  throws InterruptedException{
+public class Slides {
+    public void checkButtonPress(double target, char button_keyname) {
+
+        if (target < 0) {
+            target = 0;
+        } else if ("a".equals(button_keyname)) {
+            target = 0;
+        } else if ("x".equals(button_keyname)) {
+            target = 1065;
+        } else if ("y".equals(button_keyname)) {
+            target = 2130;
+        } else if (target > 2130) {
+            target = 2130;
+        }
+    }
+
+    public void slide(char button_keyname,
+                      double gamepad1_left_stick_y,
+                      double gamepad2_left_stick_x,
+                      double gamepad1_right_stick_x,
+                      double gamepad2_right_stick_y) {
+
+
         double target = 0;
         double difference = 0;
         DcMotor rightSlidesMotor = hardwareMap.dcMotor.get("rightSlidesMotor");
         DcMotor leftSlidesMotor = hardwareMap.dcMotor.get("leftSlidesMotor");
-        double initpos = (rightSlidesMotor.getCurrentPosition() + leftSlidesMotor.getCurrentPosition())/2;
+        double initpos = (rightSlidesMotor.getCurrentPosition() + leftSlidesMotor.getCurrentPosition()) / 2;
         rightSlidesMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         leftSlidesMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        waitForStart();
 
-        if (isStopRequested()) return;
+        double y = -(gamepad1_left_stick_y);
+        double x = gamepad2_left_stick_x;
+        double rx = gamepad1_right_stick_x;
+        double g2ry = gamepad2_right_stick_y;
+        target -= g2ry * 5;
 
-        while (opModeIsActive()){
+        checkButtonPress(target, button_keyname);
 
-            double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
-            double g2ry = gamepad2.right_stick_y;
-            target -= g2ry*5;
-            if (target < 0){
-                target = 0;
-            } else if (target > 2130){
-                target = 2130;
-            }
-            double current_pos_right = rightSlidesMotor.getCurrentPosition();
-            double current_pos_left = leftSlidesMotor.getCurrentPosition();
-            difference = target - (current_pos_left+current_pos_right)/2-initpos;
-            telemetry.addData("Pos:", (current_pos_left+current_pos_right)/2-initpos);
-            telemetry.update();
-            difference = difference * 0.01;
-            leftSlidesMotor.setPower(difference);
-            rightSlidesMotor.setPower(difference);
-        }
 
+        double current_pos_right = rightSlidesMotor.getCurrentPosition();
+        double current_pos_left = leftSlidesMotor.getCurrentPosition();
+        difference = target - (current_pos_left + current_pos_right) / 2 - initpos;
+        telemetry.addData("Pos:", (current_pos_left + current_pos_right) / 2 - initpos);
+        telemetry.update();
+        difference = difference * 0.01;
+        leftSlidesMotor.setPower(difference);
+        rightSlidesMotor.setPower(difference);
     }
+
 }
+
 //2130 Top Slide Position !!! :D
