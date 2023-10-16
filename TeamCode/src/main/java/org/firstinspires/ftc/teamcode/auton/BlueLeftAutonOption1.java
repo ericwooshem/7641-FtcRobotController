6ab87b0b3cd359606bc7357dbe3cf11b2c24f18b
Sushirahulk <113491574+Sushirahulk@users.noreply.github.com>
2023-10-15 22:32:54 -0700

    update stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueLeftAutonOption1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueLeftAutonOption1.java
@@ -1,4 +1,7 @@
 package org.firstinspires.ftc.teamcode.auton;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "BlueLeftAutonOption1")
 public class BlueLeftAutonOption1 {
 }
