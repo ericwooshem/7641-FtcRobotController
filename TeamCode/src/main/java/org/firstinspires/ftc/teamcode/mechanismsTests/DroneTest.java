99e7f0fceeb23eecb9eb211f5ad5077929ccbc3d
blueseaturtle08 <113743479+blueseaturtle08@users.noreply.github.com>
2023-10-22 16:54:22 -0700

    vision works!!!!!!!!!!!!!!!!!!!!!!1 (2)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanismsTests/DroneTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanismsTests/DroneTest.java
@@ -4,7 +4,7 @@ package org.firstinspires.ftc.teamcode.mechanismsTests;
 import org.firstinspires.ftc.teamcode.mechanisms.Drone;
 
 public class DroneTest {
-    Drone drone = new Drone();
+   // Drone drone = new Drone();
 
 
     /*  Write the code while debugging and testing the mechanism in this file */
