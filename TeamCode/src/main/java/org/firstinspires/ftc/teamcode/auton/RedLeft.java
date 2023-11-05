c26c039812bb75dc995199c83ea7a17352d16f6b
MaximeSobrier <142960631+MaximeSobrier@users.noreply.github.com>
2023-11-05 15:43:12 -0800

    rename
    rename

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/RedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/RedLeft.java
@@ -1,13 +1,10 @@
 package org.firstinspires.ftc.teamcode.auton;
-import android.transition.Slide;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.mechanisms.Slides;
 
-@Autonomous(name = "BlueRightAutonOption1")
-public class BlueRightAutonOption1 extends LinearOpMode {
+@Autonomous(name = "RedLeft")
+public class RedLeft extends LinearOpMode {
 
 
     @Override
