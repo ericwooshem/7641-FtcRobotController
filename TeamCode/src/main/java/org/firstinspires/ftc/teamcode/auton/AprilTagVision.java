15bb2520f437e27188bbe13e018146925e766729
blueseaturtle08 <113743479+blueseaturtle08@users.noreply.github.com>
2023-11-05 16:03:08 -0800

    r

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AprilTagVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AprilTagVision.java
@@ -27,12 +27,10 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.drive.opmode;
+package org.firstinspires.ftc.teamcode.auton;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
