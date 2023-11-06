9c2d84e7a77872c206644cc1aea92bc4aff66cf0
Sushirahulk <113491574+Sushirahulk@users.noreply.github.com>
2023-11-05 16:49:11 -0800

    Update RedLeft.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/RedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/RedLeft.java
@@ -37,6 +37,7 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDir
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.auton.visiontest2;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
